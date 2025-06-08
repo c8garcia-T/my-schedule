22e5214490ff353f01fa141435628947e19e58b6
c8garcia-T <18cagarcia@gmail.com>
2025-06-08 16:21:46 +0900

    config changes - parser and plugins declarations moved to compact.config - rules added (no-unused-vars (warn), no-explicit-any (warn))

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,17 +7,28 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/stylistic",
-    "prettier",
-  ),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/stylistic",
+      "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_$", // Ignore unused arguments that are exactly _
+          varsIgnorePattern: "^_$", // Ignore unused variables that are exactly _
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }),
 ];
-
 export default eslintConfig;
