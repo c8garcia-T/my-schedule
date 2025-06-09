52723db30fca1eec4c536b7983c14db9f90a8a81
c8garcia-T <18cagarcia@gmail.com>
2025-06-10 02:54:00 +0900

    feat(ui): add Schedule component to display parsed class schedule
    - Rendered each class entry with period, time, level, room, and subject
    - Used Tailwind CSS for layout and styling
    - Added divider between schedule entries

---
+++ src/ui/schedule.tsx
@@ -0,0 +1,29 @@
+import { ClassEntry } from "@/types/search-for-schedule-types";
+
+export function Schedule({ scheduleData }: { scheduleData: ClassEntry[] }) {
+  return scheduleData.map((classRecord, index) => {
+    return (
+      <div key={index + "class_record"} className="flex flex-col">
+        <span className="mb-4 flex flex-row gap-x-2 text-3xl">
+          <p>Period {classRecord.period}</p>
+          <p>{classRecord.timeInterval}</p>
+        </span>
+        <div className="flex flex-row text-xl">
+          <span className="w-1/3">
+            <p>Level</p>
+            <p>{classRecord.level}</p>
+          </span>
+          <span className="w-1/3">
+            <p>Room</p>
+            <p>{classRecord.room}</p>
+          </span>
+          <span className="w-1/3">
+            <p>Subject</p>
+            <p>{classRecord.subject}</p>
+          </span>
+        </div>
+        <div className="divider divider-primary"></div>
+      </div>
+    );
+  });
+}
