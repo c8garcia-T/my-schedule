89a3da49c1969322ea602b8860ee1058d92e0455
c8garcia-T <18cagarcia@gmail.com>
2025-06-11 01:44:56 +0900

    div container added to avoid incorrect sizing on mobile devices

--- src/ui/schedule.tsx
+++ src/ui/schedule.tsx
@@ -1,29 +1,33 @@
 import { ClassEntry } from "@/types/search-for-schedule-types";
 
 export function Schedule({ scheduleData }: { scheduleData: ClassEntry[] }) {
-  return scheduleData.map((classRecord, index) => {
-    return (
-      <div key={index + "class_record"} className="flex flex-col">
-        <span className="mb-4 flex flex-row gap-x-2 text-2xl sm:text-3xl">
-          <p>Period {classRecord.period}</p>
-          <p>{classRecord.timeInterval}</p>
-        </span>
-        <div className="flex flex-row text-xl">
-          <span className="w-1/3">
-            <p>Level</p>
-            <p>{classRecord.level}</p>
-          </span>
-          <span className="w-1/3">
-            <p>Room</p>
-            <p>{classRecord.room}</p>
-          </span>
-          <span className="w-1/3">
-            <p>Subject</p>
-            <p>{classRecord.subject}</p>
-          </span>
-        </div>
-        <div className="divider divider-primary"></div>
-      </div>
-    );
-  });
+  return (
+    <div className="w-full max-w-xl self-center sm:w-2/3">
+      {scheduleData.map((classRecord, index) => {
+        return (
+          <div key={index + "class_record"} className="flex flex-col">
+            <span className="mb-4 flex flex-row gap-x-2 text-2xl sm:text-3xl">
+              <p>Period {classRecord.period}</p>
+              <p>{classRecord.timeInterval}</p>
+            </span>
+            <div className="flex flex-row text-xl">
+              <span className="w-1/3">
+                <p>Level</p>
+                <p>{classRecord.level}</p>
+              </span>
+              <span className="w-1/3">
+                <p>Room</p>
+                <p>{classRecord.room}</p>
+              </span>
+              <span className="w-1/3">
+                <p>Subject</p>
+                <p>{classRecord.subject}</p>
+              </span>
+            </div>
+            <div className="divider divider-primary"></div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
