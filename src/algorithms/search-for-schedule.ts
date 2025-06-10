7eaadaf2d6063fa145922f761819d3e77a85a0dc
c8garcia-T <18cagarcia@gmail.com>
2025-06-11 01:33:53 +0900

    computeClassEndTime - conditional fix formatting of timeSpan refactored into function - formatClassTime feat (first class data extracted) - function: searchIsFirstClass

--- src/algorithms/search-for-schedule.ts
+++ src/algorithms/search-for-schedule.ts
@@ -5,11 +5,38 @@ function computeClassEndTime(
   minutes: number,
 ): { hour: number; minutes: number } {
   minutes = minutes + 42;
-  if (minutes > 59) {
+  if (minutes >= 60) {
     hour++;
     minutes -= 60;
   }
   return { hour: hour, minutes: minutes };
+}
+
+function formatClassTime(hour: number, minutes: number) {
+  const minutesStr = minutes.toString();
+  const minutesFrmt =
+    minutesStr.length === 1 ? minutesStr.padStart(2, "0") : minutesStr;
+  const timeStr = `${hour}:${minutesFrmt} PM`;
+  return timeStr;
+}
+
+function searchIsFirstClass(
+  classSearch: string,
+  currRowIndx: number,
+  leftIndxBoundary: number,
+  dataRef: any[][],
+): boolean {
+  let isFirst = true;
+  for (let rowIdx = 0; rowIdx < currRowIndx - 3; rowIdx++) {
+    const rowSearch = dataRef[rowIdx].slice(leftIndxBoundary);
+    const searchRes = rowSearch.indexOf(classSearch);
+    if (searchRes !== -1) {
+      isFirst = false;
+      break;
+    }
+  }
+
+  return isFirst;
 }
 
 export function searchForSchedule(data: any[][], name: string): Schedule {
@@ -20,6 +47,7 @@ export function searchForSchedule(data: any[][], name: string): Schedule {
     const nameColumnIndx = row.indexOf(name);
     let period;
     let timeInterval;
+    let isFirstClass;
     if (nameColumnIndx !== -1) {
       // Find the Period Column
       const periodRow = data[rowIdx - 3];
@@ -37,8 +65,13 @@ export function searchForSchedule(data: any[][], name: string): Schedule {
             const classEndTime = computeClassEndTime(hour, minutes);
             const endHour = classEndTime.hour;
             const endMinutes = classEndTime.minutes;
-            const endTimeStr = `${endHour}:${endMinutes.toString().padEnd(2, "0")} PM`;
-            timeInterval = `${hour}:${minutes.toString().padEnd(2, "0")} PM - ${endTimeStr}`;
+            timeInterval = `${formatClassTime(hour, minutes)} - ${formatClassTime(endHour, endMinutes)}`;
+            isFirstClass = searchIsFirstClass(
+              data[rowIdx - 3][nameColumnIndx],
+              rowIdx - 3,
+              colIdx,
+              data,
+            );
             break;
           }
         }
@@ -54,6 +87,7 @@ export function searchForSchedule(data: any[][], name: string): Schedule {
           subject: dataSlice[4][nameColumnIndx] ?? null,
           period: period,
           timeInterval: timeInterval!, // timeInterval will always be generated if period is found.
+          isFirstClass: isFirstClass!,
         };
         schedule.push(classEntry);
       } else {
