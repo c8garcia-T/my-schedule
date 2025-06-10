4838cc8c01157f52026829dac1d76e0e6a524bca
c8garcia-T <18cagarcia@gmail.com>
2025-06-11 03:01:18 +0900

    - icons added to make it a pleasant sight

--- src/ui/schedule.tsx
+++ src/ui/schedule.tsx
@@ -1,16 +1,28 @@
 import { ClassEntry } from "@/types/search-for-schedule-types";
+import { Clock, Bell, BookOpen, MapPin } from "lucide-react";
 
 export function Schedule({ scheduleData }: { scheduleData: ClassEntry[] }) {
   return (
-    <div className="w-full max-w-xl self-center sm:w-2/3">
+    <div className="w-full">
       {scheduleData.map((classRecord, index) => {
         return (
           <div key={index + "class_record"} className="flex flex-col">
-            <span className="mb-4 flex flex-row gap-x-2 text-2xl sm:text-3xl">
-              <p>Period {classRecord.period}</p>
-              <p>{classRecord.timeInterval}</p>
+            <span>
+              <span className="flex flex-row gap-x-2 text-2xl sm:text-3xl">
+                <p className="font-semibold">Period {classRecord.period}</p>
+                <div className="flex items-center gap-1">
+                  <Clock className="h-6 w-6" />
+                  <p>{classRecord.timeInterval}</p>
+                </div>
+              </span>
+              {classRecord.isFirstClass && (
+                <div className="badge">
+                  <Bell className="mr-1 h-3 w-3" />
+                  Take Attendance
+                </div>
+              )}
             </span>
-            <div className="flex flex-row text-xl">
+            <div className="mt-1 flex flex-row text-xl">
               <span className="w-1/3">
                 <p>Level</p>
                 <p>{classRecord.level}</p>
