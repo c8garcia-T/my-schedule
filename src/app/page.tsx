4328e2a588c037100b64fa5f8741c170285d83b5
c8garcia-T <18cagarcia@gmail.com>
2025-06-11 01:42:07 +0900

    css tailwind set inside Schedule Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,11 +67,7 @@ export default function Home() {
         />
       </div>
 
-      {scheduleData && (
-        <div className="w-full max-w-xl self-center sm:w-2/3">
-          <Schedule scheduleData={scheduleData} />
-        </div>
-      )}
+      {scheduleData && <Schedule scheduleData={scheduleData} />}
     </main>
   );
 }
